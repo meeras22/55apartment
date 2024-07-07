bcc5db75652754643dab4bfaee258299cf3f96ec
oyenet1 <bowofadeoyerinde@gmail.com>
2024-07-07 16:36:35 +0100

    logo

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,1 +1,9 @@
 @extends('layouts.frontend')
+@section('contents')
+    <div class="">
+        {{-- hero section --}}
+        <header class="bg-one w-full p-4">
+
+        </header>
+    </div>
+@endsection
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,8 +9,8 @@ export default {
     theme: {
         extend: {
             backgroundImage: {
-                one: "url('/img/bg-one.jpg')",
-                two: "url('/img/bg-two.jpg')",
+                one: "url('/img/bg-one.png')",
+                two: "url('/img/bg-two.png')",
             },
             fontFamily: {
                 cairo: ["Cairo"],
