392b67f33d7ec21f001ac95e6922e21c73ed4847
oyenet1 <bowofadeoyerinde@gmail.com>
2024-07-12 22:01:05 +0100

    new styling

--- resources/views/projects.blade.php
+++ resources/views/projects.blade.php
@@ -5,25 +5,29 @@
         <div
             class="max-w-screen-xl pt-12 mx-auto space-y-6 text-center bg-fixed bg-center bg-contain lg:pt-0 md:space-y-8 lg:space-y-8">
             <img src="{{ asset('img/bg-one.png') }}" alt="{{ env('APP_NAME') }}" class="block object-cover w-full h-auto">
-            <div class="w-full py-8 space-y-20 bg-fixed bg-center bg-no-repeat bg-contain lg:space-y-24"
-                style="background-image:linear-gradient(to bottom, rgba(255, 255, 255, 0.8), rgba(255, 255, 255, 0.8)), url('/img/spiral-bg.png')">
-                <div class="py-4 space-y-4 font-libre">
-                    <h1 class="pt-4 text-3xl lg:text-4xl">L.A</h1>
-                    <p class="text-2xl lg:text-3xl">
-                        <span class="font-bold">Location:</span> Jahi, Abuja
-                    </p>
-                </div>
+            <div class="w-full py-8 space-y-20 bg-fixed bg-center bg-no-repeat bg-cover lg:space-y-24">
+                <div class="py-12 space-y-6 bg-cover lg:py-24"
+                    style="background-image:linear-gradient(to bottom, rgba(255, 255, 255, 0.8), rgba(255, 255, 255, 0.8)), url('/img/spiral-bg.png')">
+                    <div class="py-4 space-y-4 font-libre">
+                        <h1 class="pt-4 text-3xl lg:text-4xl">L.A</h1>
+                        <p class="text-2xl lg:text-3xl">
+                            <span class="font-bold">Location:</span> Jahi, Abuja
+                        </p>
+                    </div>
 
-                {{-- about --}}
-                <div class="space-y-6">
-                    <h1 class="text-2xl font-libre">About L.A</h1>
-                    <p class="max-w-5xl mx-auto">Nestled in the cosmopolitan Abuja, Jahi. L.A Jahi presents a unique
-                        opportunity to experience the
-                        best of fine living. Our newest development, 'Los Angeles, boasts exclusivity and serenity, with
-                        breathtaking views of the capital. Los Angeles delivers a boutique lifestyle that is second to none.
-                        This residence provides an exclusive and safe living experience in the core of Abuja, allowing you
-                        to escape the hustle and bustle of the city and immerse yourself in the tranquillity and
-                        sophistication.</p>
+                    {{-- about --}}
+                    <div class="space-y-6">
+                        <h1 class="text-2xl font-libre">About L.A</h1>
+                        <p class="max-w-5xl mx-auto">Nestled in the cosmopolitan Abuja, Jahi. L.A Jahi presents a unique
+                            opportunity to experience the
+                            best of fine living. Our newest development, 'Los Angeles, boasts exclusivity and serenity, with
+                            breathtaking views of the capital. Los Angeles delivers a boutique lifestyle that is second to
+                            none.
+                            This residence provides an exclusive and safe living experience in the core of Abuja, allowing
+                            you
+                            to escape the hustle and bustle of the city and immerse yourself in the tranquillity and
+                            sophistication.</p>
+                    </div>
                 </div>
 
                 <div class="max-w-3xl mx-auto space-y-20 text-center">
@@ -33,7 +37,7 @@
                     <img src="{{ asset('img/gallery3.png') }}" alt="{{ env('APP_NAME') }}" class="block w-full">
                 </div>
             </div>
-            <div class="mx-auto space-y-20 text-center max-w-7xl">
+            <div class="mx-auto space-y-20 text-center bg-white max-w-7xl">
                 <h1 class="text-2xl md:text-4xl font-libre">Floor Plans</h1>
                 <img src="{{ asset('img/floor.png') }}" alt="{{ env('APP_NAME') }}" class="block w-full border-dashed">
 
