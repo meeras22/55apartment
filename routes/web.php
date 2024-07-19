069d56dcb0b1767226464d0451a576b45baff088
oyenet1 <bowofadeoyerinde@gmail.com>
2024-07-19 07:14:21 +0100

    new work done

--- routes/web.php
+++ routes/web.php
@@ -5,4 +5,5 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PageController::class, 'homepage']);
+Route::get('/test', fn () => "Hello world!");
 Route::get('/projects', [PageController::class, 'projects']);
