b1672be32fca664df6790b9aa79912b49f3d678e
oyenet1 <bowofadeoyerinde@gmail.com>
2024-07-19 08:17:11 +0100

    progress done

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -52,7 +52,7 @@
         <div class="flex flex-wrap items-center justify-between max-w-screen-xl px-4 mx-auto">
             <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
                 <img src="{{ request()->is('/') ? asset('img/whitelogo.png') : asset('img/colorlogo.png') }}"
-                    class="h-auto w-[100px] md:w-[150px] py-3 md:py-4 logo" alt="{{ env('APP_NAME') }}" />
+                    class="h-auto w-[130px] md:w-[150px] py-3 md:py-4 logo" alt="{{ env('APP_NAME') }}" />
                 {{-- <span class="self-center text-2xl font-semibold whitespace-nowrap">Flowbite</span> --}}
             </a>
             <button data-collapse-toggle="navbar-default" type="button"
