c9e88e38fa265f10b8dee037675f083d3bf2f8b1
oyenet1 <bowofadeoyerinde@gmail.com>
2024-07-22 17:59:01 +0100

    environmental variable set

--- resources/views/projects.blade.php
+++ resources/views/projects.blade.php
@@ -32,9 +32,9 @@
 
                 <div class="max-w-3xl mx-auto space-y-12 text-center md:space-y-20">
                     <h1 class="text-2xl md:text-4xl font-libre">Gallery</h1>
-                    <img src="{{ asset('img/gallery1.jpg') }}" alt="{{ env('APP_NAME') }}" class="block w-full">
-                    <img src="{{ asset('img/section2.JPG') }}" alt="{{ env('APP_NAME') }}" class="block w-full">
-                    <img src="{{ asset('img/gallery3.jpg') }}" alt="{{ env('APP_NAME') }}" class="block w-full">
+                    <img src="{{ asset('img/gly1.jpg') }}" alt="{{ env('APP_NAME') }}" class="block w-full">
+                    <img src="{{ asset('img/gallery2.jpg') }}" alt="{{ env('APP_NAME') }}" class="block w-full">
+                    <img src="{{ asset('img/gly2.jpg') }}" alt="{{ env('APP_NAME') }}" class="block w-full">
                 </div>
             </div>
             <div class="max-w-5xl mx-auto space-y-20 text-center bg-white">
